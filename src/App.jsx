8a8a5f799985fcf1f91bfb9a0cc7f8667662d2f1
Anirudha Bele <beleanirudha8@gmail.com>
2024-11-03 22:52:46 +0530

    create Counter component which is solution of problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
+import Counter from "./components/Counter";
 
 function App() {
-	return <h1>Counter App</h1>;
+	return <Counter />;
 }
 
 export default App;
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,13 +3,19 @@ import { useState } from "react";
 const Counter = () => {
 	const [counter, setCounter] = useState(0);
 
+	const incrementCounter = () => {
+		setCounter(counter + 1);
+	};
+	const decrementCounter = () => {
+		setCounter(counter - 1);
+	};
 	return (
 		<div>
 			<h1>Counter App</h1>
 			<div>
-				<button>+</button>
+				<button onClick={incrementCounter}>+</button>
 				<p>{counter}</p>
-				<button>-</button>
+				<button onClick={decrementCounter}>-</button>
 			</div>
 		</div>
 	);
